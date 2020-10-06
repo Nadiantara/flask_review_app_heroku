d674ec9df4bee103450afce9314f1efdcbbca9e4
I Wayan Nadiantara <32120463+Nadiantara@users.noreply.github.com>
2020-10-06 11:14:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ class Config:
     """Flask configuration variables."""
 
     # General Config
+    FLASK_ENV='production'
     PROPAGATE_EXCEPTIONS = True
     FLASK_APP = environ.get('FLASK_APP')
     FLASK_ENV = environ.get('FLASK_ENV')
